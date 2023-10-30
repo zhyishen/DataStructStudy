9caa3b8e5267afa1646446a5c38a644a8227ff4f
zhyishen <zhyishen@163.com>
2023-10-30 21:02:03 +0800

    SqQueue

---
+++ SqQueue.cpp
@@ -0,0 +1,38 @@
+#include<cstdio>
+#define MAXQSIZE 100;
+using namespace std;
+
+typedef struct
+{
+    
+}QElemType;
+
+typedef struct{
+    QElemType *base;
+    int front;
+    int rear;
+
+}SqQueue;
+
+
+Status InitQueue(SqQueue &Q){
+    Q.base = new QElemType[MAXQSIZE];
+    if(!Q.base) exit(OVERFLOW);
+    Q.front = Q.rear =0;
+    return OK;
+}
+
+int QueueLength(SqQueue &Q){
+
+    return (Q.rear - Q.front + MAXQSIZE)%MAXQSIZE;
+}
+
+Status EnQueue(SqQueue &Q, QElemType e){
+    if ((Q.rear+1)%MAXQSIZE == Q.front) 
+    {
+        return ERROR;
+    }
+    Q.base[Q.rear] = e;
+    Q.rear = (Q.rear +1)%MAXQSIZE;
+    return OK;
+}
